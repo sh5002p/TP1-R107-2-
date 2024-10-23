fce9efaab0565fbb341fe9dedd4361cfea3a58e3
sh5002p <151920711+sh5002p@users.noreply.github.com>
2024-10-23 15:28:41 +0200

    Delete TP2 directory

--- TP2/tp2.Exo7.py
+++
@@ -1,7 +0,0 @@
-import random as rnd
-nombre = rnd.randint(0,100)
-print(nombre, " donc c est : ")
-if nombre > 67 :
-    print("Pile !")
-else:
-    print("Face !")
--- TP2/tp2exo6.py
+++
@@ -1,7 +0,0 @@
-import random as rnd
-nombre = rnd.randint(0,100)
-print("nombre donc c est : ")   
-if nombre < 50 :
-    print("Pile !")
-else:
-    print("Face !")
